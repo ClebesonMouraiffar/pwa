3a653cc2a0576bc7b7cb08d98ba7347f8bcbf2fe
ClebesonMouraiffar <clebeson.moura@aluno.iffar.edu.br>
2019-03-18 21:10:24 -0300

    teste

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,14 @@
 const PRECAHE = 'precache-v1';
 const PRECACHE_URLS = [
     'index.html',
-    './', // Alias for index.html
+    './',
+    'app.js',
     'demo.js'
-  ];
+];
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(PRECACHE)
+            .then(cache => cache.addAll(PRECACHE_URLS))
+            .then(self.skipWaiting())
+    );
+});
